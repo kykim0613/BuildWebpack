b13b8ff73f2c7d59ac7dd28c81ac6ffe78fd6dc6
KIMYUNKI <kykim0613@gmail.com>
2023-05-26 12:34:18 +0900

    redux

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,40 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { createStore } from 'react-redux';
+
+const initialState = {
+    counter: 0,
+    text: '',
+    list: []
+}
+
+const INCREASE = 'INCREASE'
+const DECREASE = 'DECREASE'
+const CHANGE_TEXT = 'CHANGE_TEXT'
+const ADD_TO_LIST = 'ADD_TO_LIST'
+
+const increase = () => ({
+    type: INCREASE
+}
+)
+const decrease = () => ({
+    type: DECREASE
+})
+
+const changeText = text => ({
+    type: CHANGE_TEXT,
+    text
+})
+
+const addToList = item => ({
+    type: ADD_TO_LIST,
+    item
+})
 
 const Main = () => {
     const [users, setUsers] = useState(null);
-    const [error, setError] = useState(null)
+    const [error, setError] = useState(null);
+    const [getUsers, setGetUsers] = useState(false)
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -19,16 +50,33 @@ const Main = () => {
         fetchUsers();
     }, [])
     console.log(users)
+    const handleGetUsers = (e) => {
+        e.preventDefault()
+        setGetUsers(!getUsers)
+    }
     return (
         <>
             <h1>hello world!</h1>
-            <ul>
+            {/* <ul>
                 {users?.map((user) => (
                     <li key={user.id}>
                         {user.username} ({user.name})
                     </li>
                 ))}
-            </ul>
+            </ul> */}
+            <form onClick={handleGetUsers}>
+                <button>불러오기</button>
+            </form>
+            {getUsers ? (
+                <ul>
+                    {users.map((user) => (
+                        <li key={user.id}>{user.username}({user.name})</li>
+                    ))}
+                </ul>
+            ) : (
+                <></>
+            )
+            }
         </>
     )
 }
